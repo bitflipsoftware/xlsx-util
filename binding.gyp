963b141453c583f7b24a5e4ac9d6fcb813e8cfec
Matthew James Briggs <matthew.james.briggs@gmail.com>
2018-06-06 14:30:19 -0700

    getting started with node - a lot of renames here

---
+++ binding.gyp
@@ -0,0 +1,8 @@
+{
+  "targets": [
+    {
+      "target_name": "addon",
+      "sources": [ "gyp/addon.cc" ]
+    }
+  ]
+}
---
+++ gyp/index.js
@@ -0,0 +1,4 @@
+// test.js
+const addon = require('../build/Release/xlsx-util');
+
+console.log('This should be eight:', addon.add(3, 5));
