10694808600ed559e5d29a6f1efab0e6b3efc795
Matthew James Briggs <matthew.james.briggs@gmail.com>
2018-06-10 16:16:31 -0700

    save

--- cpp/xlsx.cpp
+++ cpp/xlsx.cpp
@@ -4,70 +4,71 @@
 
 namespace iq
 {
-	XlsxReader::XlsxReader( const std::string& inFilename )
-	: myIsOk{ false }
-	, myFilename{ inFilename }
-	, myReader{ nullptr }
-	{
-		myReader = xlsxioread_open( myFilename.c_str() );
+    XlsxReader::XlsxReader( const std::string& inFilename )
+    : myIsOk{ false }
+    , myFilename{ inFilename }
+    , myReader{ nullptr }
+    {
+        myReader = xlsxioread_open( myFilename.c_str() );
 
-		if( myReader != nullptr )
-		{
-			myIsOk = true;
-		}
-	}
-
-
-	XlsxReader::~XlsxReader()
-	{
-		if( myIsOk )
-		{
-			if( myReader )
-			{
-				xlsxioread_close( myReader );
-			}
-		}
-	}
+        if( myReader != nullptr )
+        {
+            myIsOk = true;
+        }
+    }
 
 
-	xlsxioreader
-	XlsxReader::getReader() const
-	{
-		return myReader;
-	}
+    XlsxReader::~XlsxReader()
+    {
+        if( myIsOk )
+        {
+            if( myReader )
+            {
+                xlsxioread_close( myReader );
+            }
+        }
+    }
 
 
-	std::vector<std::string>
-	XlsxReader::getSheetNames() const
-	{
-		std::vector<std::string> result;
-
-		if( !getIsOk() )
-		{
-			return result;
-		}
-
-		struct xlsx_list_sheets_data sheetdata;
-      // printf("Available sheets:\n");
-      // sheetdata.index = 0;
-      // sheetdata.firstsheet = NULL;
-      xlsxioread_list_sheets(myReader, xlsx_list_sheets_callback, &sheetdata);
-      // printf("Sheets found: %i\n", sheetdata.index);
-		// const char* sheetname = nullptr;
-		// if ( ( sheet = xlsxioread_sheet_open(myReader, sheetname, XLSXIOREAD_SKIP_EMPTY_ROWS ) ) != nullptr )
-		// {
-		// 	if( sheetname != nullptr )
-		// 	{
-		// 		result.push_back( std::string{ sheetname } );
-		// 	}
-		// }
-      return result;
-	}
+    int
+    XlsxReader::xlsx_list_sheets_callback( const char* name, void* callbackdata )
+    {
+      struct xlsx_list_sheets_data* data = (struct xlsx_list_sheets_data*)callbackdata;
+      // if (!data->firstsheet)
+      //   data->firstsheet = strdup(name);
+      // printf("%3i: %s\n", data->index, name);
+      // data->index++;
+      data->sheets.push_back( std::string{ name } );
+      return 0;
+    }
 
 
-	bool
-	XlsxReader::getIsOk() const
-	{
-		return myIsOk;
-	}
+    xlsxioreader
+    XlsxReader::getReader() const
+    {
+        return myReader;
+    }
+
+
+    std::vector<std::string>
+    XlsxReader::getSheetNames() const
+    {
+        std::vector<std::string> result;
+
+        if( !getIsOk() )
+        {
+            return result;
+        }
+
+        struct xlsx_list_sheets_data sheetdata;
+        xlsxioread_list_sheets(myReader, xlsx_list_sheets_callback, &sheetdata);
+        return result;
+    }
+
+
+    bool
+    XlsxReader::getIsOk() const
+    {
+        return myIsOk;
+    }
 }
--- cpp/xlsx.h
+++ cpp/xlsx.h
@@ -8,35 +8,28 @@
 
 namespace iq
 {
-	class XlsxReader
-	{
-	public:
-		XlsxReader( const std::string& inFilename );
-		~XlsxReader();
+    class XlsxReader
+    {
+    public:
+        XlsxReader( const std::string& inFilename );
+        ~XlsxReader();
 
-	public:
-		xlsxioreader getReader() const;
-		bool getIsOk() const;
-		std::vector<std::string> getSheetNames() const;
-	private:
-		bool myIsOk;
-		std::string myFilename;
-		xlsxioreader myReader;
+    public:
+        xlsxioreader getReader() const;
+        bool getIsOk() const;
+        std::vector<std::string> getSheetNames() const;
 
-	private:
-		struct xlsx_list_sheets_data {
-		  std::vector<std::string> sheets;
-		};
+    private:
+        bool myIsOk;
+        std::string myFilename;
+        xlsxioreader myReader;
 
-		static int xlsx_list_sheets_callback (const char* name, void* callbackdata)
-		{
-		  struct xlsx_list_sheets_data* data = (struct xlsx_list_sheets_data*)callbackdata;
-		  // if (!data->firstsheet)
-		  //   data->firstsheet = strdup(name);
-		  // printf("%3i: %s\n", data->index, name);
-		  // data->index++;
-		  data->sheets.push_back( std::string{ name } );
-		  return 0;
-		}
-	};
+    private:
+        static int xlsx_list_sheets_callback( const char* name, void* callbackdata );
+        struct xlsx_list_sheets_data
+        {
+          std::vector<std::string> sheets;
+        };
+
+    };
 }
