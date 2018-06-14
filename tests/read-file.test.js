bb040a390901437ddd4c4c55a580bc78367c45cf
Matthew James Briggs <matthew.james.briggs@gmail.com>
2018-06-14 10:09:19 -0700

    rewriting test

--- tests/read-file.test.js
+++ tests/read-file.test.js
@@ -8,9 +8,17 @@ describe('readFile', () => {
             return arr
         }
 
-        const filepath = path.join(__dirname, 'get-data.xlsx')
+        const filepath = path.join(__dirname, 'small.xlsx')
         const result = await xlsx.readFile(filepath)
         expect(result).toBeTruthy();
+        expect(Array.isArray(result)).toBeTruthy();
+        expect(result.length).toBe(4)
+
+        expect(result[0].A).toBe('Hello')
+        expect(result[0].B).toBe('World')
+
+
+
         done();
     })
 })
