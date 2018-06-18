d4ea518ccefec332624017a4e5a92393da65ecca
Matthew James Briggs <matthew.james.briggs@gmail.com>
2018-06-17 17:01:55 -0700

    xcode proj

---
+++ cpp/ValParseTest/ValParseTest/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  ValParseTest
+//
+//  Created by mjb on 6/17/18.
+//  Copyright © 2018 mjb. All rights reserved.
+//
+
+#include <iostream>
+#include "Val.h"
+
+int main(int argc, const char * argv[])
+{
+    xlsx::Val v;
+    std::string sci = "7.12345E-3";
+    v.setParse( sci );
+    std::cout << v.getDouble() << std::endl;
+    return 0;
+}
